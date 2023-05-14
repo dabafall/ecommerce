7ac69e1805f0509d6ccc680828f2190c57086392
dabafall <falldaba94@gmail.com>
2023-05-14 17:18:50 +0000

    integration template

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('base.html.twig');
+    }
+
+    #[Route('/home', name: 'home')]
+    public function home(): Response
+    {
+        return $this->render('home/home.html.twig');
+    }
+}
